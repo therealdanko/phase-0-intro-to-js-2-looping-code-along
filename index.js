1d5a156aee7de81c64607607d0809cea4390019a
Danko Karleusa <dankomatichevich@gmail.com>
2022-03-16 16:38:06 -0700

    commit tree be9d6c1099f77a65b25c2fcf58d8f49683f74254

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// const names = ["Guadalupe", "Ollie", "Aki"]
+const messages = []
+
+const writeCards = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+// writeCards(names);
+
+const countDown = (number) => {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
+
+countDown(10)
